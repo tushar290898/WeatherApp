7a8e130fb40fcf9bd6709c074004ea759128b274
tusharbhujade <tusharb.aobs@gmail.com>
2022-12-20 10:40:36 +0530

    all bugs solved

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from "react"
+import axios from 'axios'
+
 
 function App() {
+
+  const [data, setData] = useState({})
+  const [city, setCity] = useState("")
+ 
+  
+
+  const getWeather = (cityName) =>{
+    const appURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=ac7216f70bff06ea9b6788197d80b202`
+    axios.get(appURL)
+    .then((res)=>{
+      console.log("responseData",res.data)
+      setData(res.data)
+    }).catch((err)=>{
+      console.log("err",err)
+    })
+  }
+
+  const handleSearchCity = () =>{
+    getWeather(city);
+    setCity("")
+  }
+
+
+
+  useEffect(()=>{
+    getWeather("Delhi");
+  },[])
+ 
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src="https://environment.co/wp-content/uploads/sites/4/2022/07/diego-ph-BCuxVP5WEsU-unsplash.jpg" />
+      <div className="search-weather">
+        <input placeholder='Enter city name here..' type="text" value={city} onChange={e => setCity(e.target.value)}/>
+        <button type="button" onClick={handleSearchCity}>Search</button>
+      </div>
+      <div className="weather-card">
+        <h3>{data.name}</h3>
+        <h4>{((data?.main?.temp)-273).toFixed(2)} °C</h4>
+      </div>
     </div>
   );
 }
